5953b6c35c7bbdfae7b3632e75e39552ab8b72bf
Stephen Wan <stephen@stephenwan.net>
2016-02-20 13:17:19 -0800

    Playing with staggered motion

--- action-dropdown/app/action-dropdown.react.js
+++ action-dropdown/app/action-dropdown.react.js
@@ -1,8 +1,7 @@
 const React = require('react');
 const PropTypes = React.PropTypes;
 const ReactMotion = require('react-motion');
-const Motion = ReactMotion.Motion;
-const spring = ReactMotion.spring;
+const StaggeredMotion = ReactMotion.StaggeredMotion;
 
 const ActionDropdown = React.createClass({
 
@@ -52,32 +51,71 @@ const ActionDropdown = React.createClass({
   },
 
   render() {
-    const actions = this.props.actions.map((action, i) => {
-      const computedStyle = {
-        translateX: spring(this.state.actionsPresentation[i].translateX),
-        translateY: spring(this.state.actionsPresentation[i].translateY),
-      };
+    const spring = value => ReactMotion.spring(value, {
+      stiffness: 120,
+      damping: 17,
+    });
+
+    const defaultSprings = this.props.actions.map(() => {
+      return {
+        translateX: 0,
+        translateY: 0,
+      }
+    });
+
+    const computeSprings = prevInterpolatedStyles => {
+      const presentation = this.state.actionsPresentation;
+      return this.props.actions.map((_, i) => {
+        const goalX = presentation[i].translateX;
+        const goalY = presentation[i].translateY;
+        if (i === presentation.length - 1) {
+          return {
+            translateX: spring(goalX),
+            translateY: spring(goalY),
+          };
+        } else {
+          const forwardGoalX = presentation[i + 1].translateX;
+          const forwardCurrentX = prevInterpolatedStyles[i + 1].translateX;
+          const progressX = forwardGoalX === 0 ? 0 : forwardCurrentX / forwardGoalX;
+
+          const forwardGoalY = presentation[i + 1].translateY;
+          const forwardCurrentY = prevInterpolatedStyles[i + 1].translateY;
+          const progressY = forwardGoalY === 0 ? 0 : forwardCurrentY / forwardGoalY;
+
+          return {
+            translateX: spring(progressX * goalX),
+            translateY: spring(progressY * goalY),
+          };
+        }
+      });
+    };
+
+    const renderActions = interpolatedStyles => {
+      const renderedActions = this.props.actions.map((action, i) => {
+        const interpolatedStyle = interpolatedStyles[i];
+        const style = {
+          transform: `translateY(${ interpolatedStyle.translateY }rem) translateX(${ interpolatedStyle.translateX }rem)`,
+        };
+
+        return (
+          <div key={ action.key } style={ style } className="ActionDropdown-action">
+            { action.label }
+          </div>
+        );
+      });
 
       return (
-        <Motion key={ action.key } style={ computedStyle }>
-          {
-            style => (
-              <div style={
-                {
-                  transform: `translateY(${ style.translateY }rem) translateX(${ style.translateX }rem)`,
-                }
-              } className="ActionDropdown-action">
-                { action.label }
-              </div>
-            )
-          }
-        </Motion>
+        <div>
+          { renderedActions }
+        </div>
       )
-    });
+    };
 
     return (
       <div className="ActionDropdown" onMouseEnter={ this.onMouseEnter } onMouseLeave={ this.onMouseLeave }>
-        { actions }
+        <StaggeredMotion defaultStyles={ defaultSprings } styles={ computeSprings }>
+          { renderActions }
+        </StaggeredMotion>
       </div>
     );
   }
